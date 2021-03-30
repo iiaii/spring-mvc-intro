9536dac2f3cfa5137f51daf42dbbd4c6b65cc3a9
iiaii <jhun9409@naver.com>
2021-03-30 23:52:15 +0900

    회원관리 예제 기본 테스트 코드 추가

---
+++ src/test/java/me/iiaii/springmvcintro/servlet/repository/MemberRepositoryTest.java
@@ -0,0 +1,54 @@
+package me.iiaii.springmvcintro.servlet.repository;
+
+import me.iiaii.springmvcintro.servlet.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    @DisplayName("저장")
+    public void save() throws Exception {
+        // given
+        Member member = new Member("hello", 20);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+    
+    @Test
+    @DisplayName("모두찾기")        
+    public void findAll() throws Exception {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
