25212b54b1bc43e025142a80cc06b99bcd86f208
iiaii <jhun9409@naver.com>
2021-04-03 18:13:03 +0900

    front controller 패턴으로 view 분리

---
+++ src/main/java/me/iiaii/springmvcintro/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -0,0 +1,28 @@
+package me.iiaii.springmvcintro.servlet.web.frontcontroller.v1.controller;
+
+import me.iiaii.springmvcintro.servlet.domain.Member;
+import me.iiaii.springmvcintro.servlet.repository.MemberRepository;
+import me.iiaii.springmvcintro.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
