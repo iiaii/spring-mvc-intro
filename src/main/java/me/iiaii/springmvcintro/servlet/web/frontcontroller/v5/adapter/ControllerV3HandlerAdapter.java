d416440949f52b6dcd95ed65153934199913352c
iiaii <jhun9409@naver.com>
2021-04-04 23:05:29 +0900

    v3 컨트롤러에 어댑터 패턴 적용

--- src/main/java/me/iiaii/springmvcintro/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
+++ src/main/java/me/iiaii/springmvcintro/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -19,8 +19,8 @@ public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
     }
 
     @Override
-    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object hanler) throws ServletException, IOException {
-        ControllerV3 controller = (ControllerV3) hanler;
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV3 controller = (ControllerV3) handler;
 
         Map<String, String> paramMap = createParamMap(request);
         ModelView mv = controller.process(paramMap);
