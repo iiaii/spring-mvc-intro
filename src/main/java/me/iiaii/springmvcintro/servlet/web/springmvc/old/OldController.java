e29cb2a2483252efe462e7939148c9a80482440e
iiaii <jhun9409@naver.com>
2021-04-05 23:29:13 +0900

    [springMVC] 기본 구조 및 HandlerMapping(Request Handler) 구현

---
+++ src/main/java/me/iiaii/springmvcintro/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,18 @@
+package me.iiaii.springmvcintro.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/springmvc/old-controller") // BeanNameUrlHandlerMapping (스프링 빈의 이름으로 핸들러를 찾는다)
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
