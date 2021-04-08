943131b43a8b0e119186bef4bd6ab6a42779798f
iiaii <jhun9409@naver.com>
2021-04-08 23:52:04 +0900

    로그 레벨 설정 및 연산 없이 출력하는 방법 주석 추가

--- src/main/java/me/iiaii/springmvcintro/project/basic/LogTestController.java
+++ src/main/java/me/iiaii/springmvcintro/project/basic/LogTestController.java
@@ -1,21 +1,23 @@
 package me.iiaii.springmvcintro.project.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+//    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/log-test")
     public String logTest() {
         String name = "Spring";
 
         System.out.println("name = " + name);
-        log.trace("log={}", name);
+        log.trace("log={}", name);      // +를 사용하면 연산이 일어나기 때문에 ,를 사용해야함
         log.debug("log={}", name);
         log.info("log={}", name);
         log.warn("log={}", name);
